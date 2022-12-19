f5af8a28f72b668fc9c7fa63a3d5660e7837f9ec
Daniel Moraes <96148567+SirDnx@users.noreply.github.com>
2022-12-19 00:53:56 -0300

    gitignore atualizado, teste

--- excecoes/src/com/exceptionex/ExcecaoValorInvalido.java
+++
@@ -1,7 +0,0 @@
-package com.exceptionex;
-
-public class ExcecaoValorInvalido extends Exception{
-    public ExcecaoValorInvalido (double valor){
-        super("O valor a ser depositado deve ser superior a R$ 0.00");
-    }
-}
